ea98118db6dd5caa0fc6611db5f57fb6328b6b77
Renato Picco <56326785+repicco@users.noreply.github.com>
2020-01-04 05:13:27 -0300

    Mod.280
    DevCompleto.Es6.Mod.280

--- AppOrcamento/app.js
+++ AppOrcamento/app.js
@@ -7,7 +7,45 @@ class Despesa {
         this.descricao = descricao
         this.valor = valor
     }
+    
+    validarDados(){
+        for(let i in this) {
+            if(this[i] == undefined || this[i] == '' || this[i] == null) {
+                return false
+            }
+        }
+        return true
+    }
 }
+
+class Bd {
+    constructor() {
+        let id = localStorage.getItem('id')
+
+        if(id === null) {
+            localStorage.setItem('id', 0)
+        }
+    }
+    getProximoId() {
+        let proximoId = localStorage.getItem('id') //null
+        return parseInt(proximoId) + 1
+    }
+
+    gravar(d) {
+        //JSON.stringify para transformar Objeto em JSON 
+        //ex Obj > {preco: 1, valor: 2}
+        //ex JSON > '{"preco": 1, "valor": 2}' 
+        //JSON.parse para retornar JSON ao Objeto
+
+        let id = this.getProximoId()
+
+        localStorage.setItem(id, JSON.stringify(d))
+        
+        localStorage.setItem('id', id)
+    }
+}
+
+let bd = new Bd()
 
 function cadastrarDespesa(){
 
@@ -19,12 +57,34 @@ function cadastrarDespesa(){
     let valor =  document.getElementById('valor')
 
     let despesa = new Despesa(
-        ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value
+        ano.value,
+        mes.value,
+        dia.value,
+        tipo.value,
+        descricao.value,
+        valor.value
     )
+    
+    if(despesa.validarDados()){
+        bd.gravar(despesa)
+        //dialog Sucesso
+        document.getElementById('modal_titulo').innerHTML = 'Sucesso na gravação!'
+        document.getElementById('modal_text').className = 'modal-header text-success'
+        document.getElementById('modal_conteudo').innerHTML = 'Despesa inserida com sucesso!' 
+        document.getElementById('modal_botao').innerHTML = 'Ok'
+        document.getElementById('modal_botao').className = 'btn btn-success'
 
-    gravar(despesa)
-}
+        $('#modalRegistraDespesa').modal('show')
+    } else {
+        //dialog Erro
+        document.getElementById('modal_titulo').innerHTML = 'Erro na gravação'
+        document.getElementById('modal_text').className = 'modal-header text-danger'
+        document.getElementById('modal_conteudo').innerHTML = 'Existem campos obrigatórios que não foram preenchidos'  
+        document.getElementById('modal_botao').innerHTML = 'Voltar e Corrigir'
+        document.getElementById('modal_botao').className = 'btn btn-danger'
 
-function gravar(d) {
-    localStorage.setItem('despesa', JSON.stringify(d))
+        $('#modalRegistraDespesa').modal('show')
+        
+    }
+    
 }
